a3b0095e18405eea5d2b801f5dd5add5fa7b8502
NanoScavatoreee <165329811+NanoScavatoreee@users.noreply.github.com>
2024-03-28 21:45:08 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Bot ufficiale di DwarfMC!"];
 
 
 let currentIndex = 0;
